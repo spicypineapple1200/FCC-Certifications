87d69d5dbe67f124c20c72c4ee1596355d5340e1
Brendan Grigsby <141603988+spicypineapple1200@users.noreply.github.com>
2025-10-07 16:12:56 -0500

    Update solution.py

--- Scientific Computing with Python/Arithmetic Formatter/solution.py
+++ Scientific Computing with Python/Arithmetic Formatter/solution.py
@@ -22,4 +22,9 @@ def arithmetic_arranger(problems, show_answers=False):
 
 arithmetic_arranger(["32 - 698", "1 - 3801", "45 + 43", "123 + 49", "988 + 40"], True)
 
-# arithmetic_arranger(["3 / 855", "3801 - 2", "45 + 43", "123 + 49"])
+
+# Situations that will return an error:
+# If there are too many problems supplied to the function. The limit is five, anything more will return: 'Error: Too many problems.'
+# The appropriate operators the function will accept are addition and subtraction. Multiplication and division will return an error. Other operators not mentioned in this bullet point will not need to be tested. The error returned will be: "Error: Operator must be '+' or '-'."
+# Each number (operand) should only contain digits. Otherwise, the function will return: 'Error: Numbers must only contain digits.'
+# Each operand (aka number on each side of the operator) has a max of four digits in width. Otherwise, the error string returned will be: 'Error: Numbers cannot be more than four digits.'
