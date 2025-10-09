5dea1e6b12199114d22db571bcbc5296445f9199
Brendan Grigsby <141603988+spicypineapple1200@users.noreply.github.com>
2025-10-09 15:37:16 -0500

    Update solution.py

--- Scientific Computing with Python/Time Calculator (In Progress)/solution.py
+++ Scientific Computing with Python/Time Calculator (In Progress)/solution.py
@@ -1,55 +1,33 @@
-from datetime import datetime, timedelta
-
 def add_time(start, duration, day=None):
+    end_bit = start.split(" ")[1]
+    new_time = start.split(" ")[0]
+    nt1 = int(new_time.split(":")[0])+int(duration.split(":")[0])
+    nt2 = int(new_time.split(":")[1])+int(duration.split(":")[1])
+    print(nt1, nt2)
+    days_passed = 0
+    while nt2 >= 60:
+        nt2-=60
+        nt1+=1
+    while nt1 > 12:
+        nt1-=12
+        if end_bit == "AM": end_bit = "PM"
+        else: 
+            end_bit = "AM"
+            days_passed+=1
+    answer = f"{nt1}:{nt2} {end_bit}"
 
-    # Code for getting new time
-    time_format = "%I:%M %p"
-    initial_time = datetime.strptime(start, time_format)
-    
-    hours = int(duration.split(":")[0])
-    minutes = int(duration.split(":")[1])
-    days = str((hours+minutes)/24)
-    
-    duration_to_add = timedelta(hours=hours, minutes=minutes)
-    final_time = initial_time + duration_to_add
-    f = [item for item in final_time.strftime(time_format)]
-    answer = ""
-    for item in f: answer+=item
-    if answer[0] == "0":
-        answer = answer[1:]
-
-    # Code for getting new day of the week
-    week_days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
     if day:
         day = day.lower()
-        if day not in week_days:
-            pass
-        else:
-            index = week_days.index(day)
-            for num in range(int(float(days))):
-                index+=1
-                if index == 7:
-                    index = 0
-            new_day = week_days[index]
-            answer+=", "+new_day.title()
-
-    # Code for getting days later bit
-    print(days)
-    if float(days) > 1.0:
-        add = int(days.split(".")[0])+1
-        day_statement = f"({add} days later)"
-        answer+=" "+day_statement
-    elif float(days) > 0.99:
-        day_statement = "(next day)"
-        answer+=" "+day_statement
-    else:
-        pass
-    
-    
+        week_days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+        num = (week_days.index(day))+days_passed
+        while num > 6:
+            num-=7
+        day = week_days[num].title()
+        answer+=f", {day}"
+    if days_passed == 1: answer+=" (next day)"
+    elif days_passed > 1: answer+=f" ({days_passed} days later)"
+    else:pass
     print(answer)
 
-add_time('3:30 PM', '2:12', 'Monday')
-# should return '5:42 PM, Monday'.
-
-add_time('8:16 PM', '466:02', 'tuesday')
-# should return '6:18 AM, Monday (20 days later)'.
+add_time('11:59 PM', '24:05', 'Wednesday')
+# should return '12:04 AM, Friday (2 days later)'.
